ba4484211034cbcf6fdfbd11b659862779bf38c5
Ihasiniravo "RIME <silverlion2424@gmail.com>
2024-02-08 22:05:54 +0000

    fonction parfait, pas de vérification

--- tp.js
+++ tp.js
@@ -1,3 +1,6 @@
+let a = 497;
+premier(a);
+parfait(a);
 
 function premier(num){
     let count = 0;
@@ -12,7 +15,18 @@ function premier(num){
     else console.log(num+" n'est pas premier"+count+" diviseurs");
 }
 function parfait(num){
-    
+    let count = 0;
+    for(let i = 1;i<num;i++){
+        if(num%i===0){
+            count+=i;
+        }
+    }
+    console.log("count "+count);
+    if (count===num) {
+        console.log(num+" est parfait! "+count+" somme");
+    }else{
+        console.log(num+" n'est pas parfait"+count+" somme");
+    }
 }
 function nbPresencePremiers(numArray){
     
